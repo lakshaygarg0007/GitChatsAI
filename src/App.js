11c4b4cf9cadafab3be27c7b337c4f7cd0a5237e
lakshaygarg0007 <lgarg_be17@thapar.edu>
2023-10-23 16:54:13 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './App.css'
+import HomePage from "./HomePage";
+import MyModel from "./MyModel";
 
 function App() {
+  const [repoUrl, setRepoUrl] = useState('');
+
+  const handleRepoUrlChange = (event) => {
+    setRepoUrl(event.target.value);
+  };
+
+  const handleTrainModel = () => {
+    // You can add logic here to send the repo URL to the server for training the model.
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router>
+       <Routes>
+         <Route path="/" element={<HomePage />} />
+         <Route path="/models" element={<MyModel />} />
+       </Routes>
+     </Router>
   );
 }
 
